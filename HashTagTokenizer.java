fcc604445ff64e19e8b99fd7a02ecf6c3385db12
inbarBenjo <154405262+inbarBenjo@users.noreply.github.com>
2024-02-01 21:06:38 +0200

    test1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -6,35 +6,59 @@ public class HashTagTokenizer {
 
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
+	//	System.out.println(existInDictionary(hashTag , dictionary));
+     // hashTag= hashTag.toLowerCase();
 		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
 
-		// Your code here
+		for ( int i =0; i < 3000 ; i ++)
+		{
+			dictionary[i]= in.readLine();
+		}
 
 		return dictionary;
 	}
 
-	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+	public static boolean existInDictionary(String word, String []dictionary) 
+	{
+		boolean find = false; 
+		for ( int i = 0; i < 3000; i ++)
+		{
+			if (dictionary[i].equals(word))
+			{
+				find =  true; 
+			}
+		}
+        return find; 
 	}
 
-	public static void breakHashTag(String hashtag, String[] dictionary) {
+	public static void breakHashTag(String hashtag, String[] dictionary)
+	 {
 
-		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
+        if (hashtag.isEmpty()) 
+		{
             return;
         }
- 
-        int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
 		
+        hashtag = hashtag.toLowerCase(); 
+        int N = hashtag.length();
+		
+        for (int i = 1; i <= N; i++) 
+		{	
+			String hasNew = hashtag.substring(0,i);
+			
+            if (existInDictionary(hasNew, dictionary) == true)
+			{
+				System.out.println(hasNew);
+				breakHashTag(hashtag.substring(i), dictionary);
+				return;
+			}							
         }
+		
     }
 
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -8,28 +8,105 @@ public class SpellChecker {
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
+        // String str = "inbar" ;
+		// System.out.println(tail(str));
+		// String str1 = "love";
+		// String str2 = "i";
+		// System.out.println(levenshtein(str1,str2));
+
 	}
 
-	public static String tail(String str) {
-		// Your code goes here
+	public static String tail(String str)
+	 {
+		if (str.length() == 1)
+		{
+           return null ;
+		}
+		String strNew = "";
+		for ( int i =1; i < str.length(); i ++ )
+		{
+			strNew = strNew + str.charAt(i) ; 
+		}
+         return strNew ; 
 	}
 
-	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+	public static int levenshtein(String word1, String word2) 
+	{   
+        if ( word1 == null)
+		{
+           return (word2 != null) ? word2.length() : 0 ; 
+		}
+
+		if (word2 == null)
+		{
+			return word1.length();
+		}
+
+		if ( word1.length() == 0 )
+		{
+			return word2.length(); 
+		}
+        if (word2.length() == 0)
+		{
+			return word1.length();
+		}
+		if (word1.charAt(0) == word2.charAt(0))
+		{
+			return levenshtein(tail(word1), tail(word2));
+		}
+		
+	    int case1 = levenshtein(tail(word1), word2);
+		int case2 = levenshtein(word1, tail(word2));
+		int case3 = levenshtein(tail(word1), tail(word2));
+
+        return (1 + (Math.min(Math.min(case1, case2), case3))) ;
+			
+
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
 
-		// Your code here
+		for ( int i =0; i < 3000 ; i ++)
+		{
+			dictionary[i]= in.readLine();
+		}
 
 		return dictionary;
 	}
 
-	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+	public static String spellChecker(String word, int threshold, String[] dictionary)
+    {
+		String similar = ""; 
+		int min = threshold;
+		String similar2 = " "; 
+		for (int i =0; i < dictionary.length ; i ++)
+		{
+			if (levenshtein(dictionary[i], word) <= threshold)
+			{   
+                similar2 = dictionary[i]; 
+                if (levenshtein(similar2, word) < min)
+				{
+					similar = dictionary[i]; 
+					min = levenshtein(dictionary[i], word);
+				}
+			}
+		}
+		if ( "".equals(similar2))
+		{
+			return word;
+		}
+		if ("".equals(similar))
+		{
+           return similar2; 
+		}
+		else 
+		{
+			return similar; 
+		}
+
+		
 	}
 
 }
